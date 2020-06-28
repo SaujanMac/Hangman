63c4c199d93a75daec6645adc7167769e17d1833
SaujanMac <sauzan1897@gmail.com>
2020-06-28 11:34:05 +0545

    all finish

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ UIQuestionForm.addEventListener('submit', function(a) {
     answerBoxUI.innerHTML = s; //printing that string on HTML DOM
 });
 
-UIform.addEventListener('submit', function (b) {
+UIform.addEventListener('submit', function(b) {
     b.preventDefault();
     let insertChar = enterLetterBox.value;
     console.log('The length of the input is ' +enterLetterBox.value.length);
@@ -86,23 +86,83 @@ UIform.addEventListener('submit', function (b) {
     const questionString = JSON.stringify(letters);
     console.log(letters);
     console.log(typeof(questionString));
+    
+    //When the answer matches the question
     if (answerString === questionString) {
         remTries.parentElement.style.display='none';
         $(".enterLetter").hide();
         document.querySelector('input.btn').value='WINNER';
         document.querySelector('input.btn').disabled='true';
+        document.querySelector('.changeQ').style.display = "none";
+        document.querySelector('.playAgain').style.display="inline-block";
+        document.querySelector('.content2 h1').textContent="CONGRATULATIONS"
+        document.querySelector('.content2').style.margin = "100px 50px";
+        document.querySelector('.content2').style.transition = '0.5s';
     }
     
 });
 
-function checkLetter(a) {
-    if (enterLetterBox.value.length > 1) {
-        enterLetterBox.value="";
-        const warn = document.createElement('div');
-        warn.className = 'warning';
-        warn.innerText="You can only input 1 letter at a time.";
-    }
-}
+// // Trial and it WORKED!!!// //For one and i don't know what happened so FUCK THIS!!//
+// UIform.addEventListener('keypress', function (c) {
+//     if (enterLetterBox.value.length > 0) {
+//         alert ("only one letter allowed");
+//         enterLetterBox.value="";
+//     }
+//     else if (enterLetterBox.value.length == 0) {
+//         b.preventDefault();
+//     let insertChar = enterLetterBox.value;
+//     console.log('The length of the input is ' +enterLetterBox.value.length);
+//     //check if the letter is in the letters array or not
+//     if (letters.includes(insertChar) == false) {
+//         console.log('not in array');
+//         maxTries-=1;
+//         remTries.textContent = `${maxTries}`;
+//     }
+            
+//     //check if the letters is already included
+            
+//     if (answerArr.includes(insertChar) == true) {
+//                 message.textContent = 'The letter is already included';
+//     }
+
+//     letters.forEach(function (i, index) {
+//         // console.log(i, index);
+//         console.log(i.indexOf(insertChar));
+//         if (i.indexOf(insertChar) === 0) {
+//             // answerArr.splice(index,count, insertChar);
+//             answerArr[index]= insertChar; // print the insertChar value on the respective index no. of answerArr.
+//         }
+//     });
+
+//     // If tries are over
+//     if (maxTries === 0) {
+//         remTries.parentElement.textContent = 'Game Over';
+//         enterLetterBox.disabled = true;
+//     }
+//     console.log(answerArr);
+//     let answerstore = answerArr.join(" ");
+//     answerBoxUI.innerHTML = answerstore;
+//     enterLetterBox.value = '';
+//     const answerString = JSON.stringify(answerArr);
+//     console.log(answerString);
+//     console.log(typeof(answerString));
+//     const questionString = JSON.stringify(letters);
+//     console.log(letters);
+//     console.log(typeof(questionString));
+//     if (answerString === questionString) {
+//         remTries.parentElement.style.display='none';
+//         $(".enterLetter").hide();
+//         document.querySelector('input.btn').value='WINNER';
+//         document.querySelector('input.btn').disabled='true';
+//         document.querySelector('.changeQ').textContent="Play Again";
+//         document.querySelector('.content2 h1').textContent="CONGRATULATIONS"
+//         document.querySelector('.content2').style.margin = "100px 50px";
+//         document.querySelector('.content2').style.transition = '0.5s';
+//     }
+    
+//     }
+
+// });
 
 //Messaging function
 // function setMessage(msg) {
